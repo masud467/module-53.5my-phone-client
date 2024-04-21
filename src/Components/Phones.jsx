9cd9e1c07fc5b74d3b2fd99441d1475d2ae65b48
masud4657 <masudpfrnt@gmail.com>
2024-04-21 16:33:29 +0600

    first commit

---
+++ src/Components/Phones.jsx
@@ -0,0 +1,19 @@
+import { Link, useLoaderData } from "react-router-dom";
+
+
+const Phones = () => {
+
+    const phones = useLoaderData()
+    return (
+        <div>
+            <h1>All phones here : {phones.length}</h1>
+            <div>
+                {
+                    phones.map(phone=> <li key={phone.id}><Link to={`/phone/${phone.id}`}>{phone.name}</Link></li>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Phones;
